322396029604447552cab8dc33a9edbb4736ed3a
Laurel <laureloneill532@gmail.com>
2025-05-22 13:49:31 -0400

    d

--- psf_cut_app.py
+++ psf_cut_app.py
@@ -8,12 +8,19 @@ from pathlib import Path
 import numpy as np
 from lmfit.models import GaussianModel
 from scipy.interpolate import griddata
-import sys, getopt
+import sys, getopt,os
 
 
 arglist = sys.argv[1:]
 
 data_dir = Path(arglist[0])
-write_loc = Path(arglist[1])
+#write_loc = Path(arglist[1])
 
 folder_list = [folder for folder in os.listdir(data_dir) if os.path.isdir(os.path.join(data_dir, folder))]
+#dir_list = os.listdir(data_dir)
+#print(folder_list)
+run = np.zeros(len(folder_list))
+for a in range(len(folder_list)):
+    run[a], anode[a], runtype[a], temp[a], sweep[a], vTheta[a], hTheta[a] =  folder_list[a].split('_')
+
+print(htheta)
