53835ac55f66455b72f27198a56762e86acb0ae8
laureloneill <adoneill03@gmail.com>
2025-09-12 13:13:35 -0400

    added ability to fit using eventlist directly

--- bcpsf.py
+++ bcpsf.py
@@ -8,6 +8,8 @@ import os
 import scipy.stats as stats
 import json
 import pandas as pd
+from xraysky.scripts import events_imaging
+
 
 def get_peaksig_json(json_path):
     with open(json_path, 'r') as file:
@@ -80,7 +82,7 @@ def distFromCentroid(observation,plotHist=False):
         plt.hist(dist)
     return dist
 
-def analyze_localization(data_dir,window=10,plot = False ):
+def analyze_localization_directory(data_dir,window=10,plot = False ):
     data_dir = Path(data_dir)
     # take the names of all folders within the specified directory
     folder_list = [folder for folder in os.listdir(data_dir) if os.path.isdir(os.path.join(data_dir, folder))]
@@ -97,20 +99,21 @@ def analyze_localization(data_dir,window=10,plot = False ):
     pval = [np.nan]*len(folder_list) 
 
     for a in range(len(folder_list)): 
-        '''
-        numTerms = len(folder_list[a].split('_'))
-        if numTerms == 7:
-            run[a], anode[a], detectorMode[a], temp[a], sweep[a], vTheta[a], hTheta[a] =  folder_list[a].split('_')
+        #
+        #
+        # numTerms = len(folder_list[a].split('_'))
+        #if numTerms == 7:
+        #    run[a], anode[a], detectorMode[a], temp[a], sweep[a], vTheta[a], hTheta[a] =  folder_list[a].split('_')
 
-        elif numTerms == 6:
-            run[a], anode[a], detectorMode[a], temp[a], sweep[a], vTheta[a] = folder_list[a].split('_')
-            vTheta[a] = 'NA'
-            hTheta[a] = 'NA'
-        elif numTerms == 4:
-            run[a], anode[a], detectorMode[a], temp[a] = folder_list[a].split('_') 
-        elif numTerms == 3:
-            run[a],vTheta[a], hTheta[a] = folder_list[a].split('_')
-        '''
+        #elif numTerms == 6:
+        #    run[a], anode[a], detectorMode[a], temp[a], sweep[a], vTheta[a] = folder_list[a].split('_')
+        #    vTheta[a] = 'NA'
+        #    hTheta[a] = 'NA'
+        #elif numTerms == 4:
+        #    run[a], anode[a], detectorMode[a], temp[a] = folder_list[a].split('_') 
+        #elif numTerms == 3:
+        #    run[a],vTheta[a], hTheta[a] = folder_list[a].split('_')
+        #
         if os.path.isfile(f"{data_dir}/{folder_list[a]}/Analysis/imaging_analysis/Figures/imaging_source.png"):
             data_path = Path(f"{data_dir}/{folder_list[a]}/Analysis/imaging_analysis/image_reconstruction.fits.gz")
             try:
@@ -149,12 +152,14 @@ def analyze_localization(data_dir,window=10,plot = False ):
                 validPeak[a] = "corrupt Fits"
         else:
             validPeak[a] = False  
+        ######## Adjust whats in between the ########################################
         if os.path.isfile(f"{data_dir}/{folder_list[a]}/Analysis/imaging_analysis/analysis_results.json"):
             json_path = Path(f"{data_dir}/{folder_list[a]}/Analysis/imaging_analysis/analysis_results.json")
             try:
                 peakSig[a] = get_peaksig_json(json_path)
             except:
                 print('no peak')
+        ###########################################################################
     result = {
         "xCenter" : xMean,
         "yCenter" : yMean,
@@ -167,4 +172,75 @@ def analyze_localization(data_dir,window=10,plot = False ):
     }
 
     result = pd.DataFrame(result)
+    return result
+
+
+def analyze_localization(eventlistin ,window=10,plot = False ):
+    if type(eventlistin) == "str":
+        eventlistin = fits.open(Path(eventlistin))
+        data = d[0].data
+        #w = WCS(d[0].header)
+
+    if isinstance(eventlistin,(fits.HDUList)):
+        data = eventlistin[0].data # data contents of the fits file
+        #w = WCS(eventlistin[0].header)
+
+    if isinstance(eventlistin,(fits.BinTableHDU)):
+        data = eventlistin.data # data contents of the 
+    
+
+    ny, nx = data.shape
+    ym, xm = np.mgrid[:ny, :nx]
+    
+    if isinstance(eventlistin,(fits.HDUList)):
+        w = WCS(eventlistin[0].header)
+    elif isinstance(eventlistin,(fits.BinTableHDU)):
+        w = WCS(eventlistin.header)
+    else:
+        message = "Something about eventlistin is incorrect, couldnt assign w properly"
+        print(message)
+        return 
+    
+    x,y= w.array_index_to_world_values(ym,xm)
+    x = (x+(180+360)) % 360 -180
+
+    peak = np.max(data)
+    peak_loc = np.unravel_index(np.argmax(data),data.shape)
+    window = 10
+
+    windowed_data = data[peak_loc[0]-window:peak_loc[0]+window, peak_loc[1]-window:peak_loc[1]+window]
+    x_window = x[peak_loc[0]-window:peak_loc[0]+window, peak_loc[1]-window:peak_loc[1]+window]
+    y_window = y[peak_loc[0]-window:peak_loc[0]+window, peak_loc[1]-window:peak_loc[1]+window]
+
+    fitted,covariance_matrix = fit_rotated_2d_gaussian(windowed_data, x_window, y_window, plot_result=plot)
+
+    validPeak = True
+    xMean = fitted.x_mean.value
+    yMean = fitted.y_mean.value
+    xSigma = fitted.x_stddev.value
+    ySigma = fitted.y_stddev.value
+    Amplitude = fitted.amplitude.value
+    theta =fitted.theta.value
+    cov = covariance_matrix
+    
+    imager= events_imaging.BC_Imaging()
+    peaks = imager.imager.findpeaks(data) # Index 5 has the significance
+    if len(peaks) == 1: 
+        peakSig = peaks[5]
+    if len(peaks) == 2:
+        peakSig = peaks[0]
+        peakSig = peakSig[5]
+
+    result = {
+        "xCenter" : xMean,
+        "yCenter" : yMean,
+        "xSigma" : xSigma,
+        "ySigma" : ySigma,
+        "peak" : Amplitude,
+        "peakSig" : peakSig,
+        "theta" : theta,
+        'covMatrix' : cov
+    }
+
+    #result = pd.DataFrame(result)
     return result
